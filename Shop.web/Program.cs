47942d3af85052387dfa5ba76b074a5fa8fee9de
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-06-28 18:27:00 +0600

    configured autofac and serilog

--- Shop.web/Program.cs
+++ Shop.web/Program.cs
@@ -1,10 +1,22 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac; 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Shop.web.Data;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//configure host_specific_properties (By customizing host-specific properties, you ensure your application is optimized for its specific hosting environment, be it local development, staging server, or production deployment.)
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => {
+    containerBuilder
+        .RegisterModule(new WebModule());
+});
+
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -14,30 +26,58 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Host.UseSerilog((ctx, lc) => lc
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+try
 {
-    app.UseMigrationsEndPoint();
+    Log.Information("Application Starting Up");
+
+    var app = builder.Build();
+    
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization(); //this middleware verifies the identity of the user making the request.
+
+    app.UseAuthorization(); //this middleware checks if the incoming request has the necessary permissions to access the requested resource.
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapRazorPages();
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
 
-app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
-app.Run();
+
---
+++ Shop.web/WebModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Autofac.Core;
+
+internal class WebModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        //builder.RegisterType<TestClass>().As<ITestClass1>().InstancePerLifetimeScope(); 
+          /*The code registers TestClass as the implementation of ITestClass1 in Autofac,
+           *so when ITestClass1 is requested, Autofac will provide an instance of TestClass.*/
+
+        //builder.RegisterType<IndexModel>().AsSelf(); //if wish to pass a model throgh DI
+        /* RegisterType<IndexModel>(), it registers the IndexModel class as a dependency with Autofac.
+         * .AsSelf(), it specifies that, Autofac will provide an instance of IndexModel when requested */
+
+        base.Load(builder);
+    }
+}
