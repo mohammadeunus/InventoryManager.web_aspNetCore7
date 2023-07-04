7da2c1b9410df9cd4e88304410ed3aa1fb84e71d
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-07-05 01:53:49 +0600

    added exception handling

--- Shop.web/Controllers/ProductModelsController.cs
+++ Shop.web/Controllers/ProductModelsController.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,53 +24,69 @@ namespace Shop.web.Controllers
         // GET: ProductModels
         // GET: /Product/ProductEntry
         public IActionResult ProductEntry()
-        { 
+        {
             return View();
         }
-         
+
         public IActionResult SaveProduct(ProductModel model)
         {
-            _logger.LogInformation("ProductModels/SaveProduct action was called.");
-
-            // Set the current time for CreatedDate and UpdatedDate properties
-            model.CreatedDate = DateTime.Now;
-            model.CreatedBy = "Eunus";
-            model.CategoryId = 1;
-            ModelState.Clear();
-
-
-            if (ModelState.IsValid)
+            try
             {
-                // Process and save the Product data
-                _context.products.Add(model);
-                _context.SaveChanges();
+                _logger.LogInformation("ProductModels/SaveProduct action was called.");
 
-                // Redirect to a different page after successful submission
-                return RedirectToAction("index");
-            }
-            if (!ModelState.IsValid)
-            {
-                _logger.LogInformation("ModelState is not Valid: In ProductModels/SaveCategory.");
-                foreach (var modelStateEntry in ModelState.Values)
-                {
-                    foreach (var error in modelStateEntry.Errors)
-                    {
-                        _logger.LogInformation("ModelState is not Valid: "+ error.ErrorMessage); 
-                    }
-                }
+                // Set the current time for CreatedDate and UpdatedDate properties
+                model.CreatedDate = DateTime.Now;
+                model.CreatedBy = "Eunus";
+                model.CategoryId = 1;
                 ModelState.Clear();
+
+                if (ModelState.IsValid)
+                {
+                    // Process and save the Product data
+                    _context.products.Add(model);
+                    _context.SaveChanges();
+
+                    // Redirect to a different page after successful submission
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    _logger.LogInformation("ModelState is not valid: In ProductModels/SaveCategory.");
+                    foreach (var modelStateEntry in ModelState.Values)
+                    {
+                        foreach (var error in modelStateEntry.Errors)
+                        {
+                            _logger.LogInformation("ModelState is not valid: " + error.ErrorMessage);
+                        }
+                    }
+                    ModelState.Clear();
+                }
+
+                // If the model state is not valid, return to the ProductEntry view with validation errors
+                return View("ProductEntry", model);
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred in ProductModels/SaveProduct");
+                // Optionally, you can handle the exception and show an appropriate error message to the user
+                return View("Error");
+            }
+        }
 
-
-            // If the model state is not valid, return to the ProductEntry view with validation errors
-            return View("ProductEntry", model);
+        public IActionResult Index()
+        {
+            try
+            {
+                return View(_context.products);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred in ProductModels/Index");
+                // Optionally, you can handle the exception and show an appropriate error message to the user
+                return View("Error");
+            }
         }
 
 
-        public IActionResult index()
-        {
-            return View(_context.products);
-        } 
-        
     }
 }
