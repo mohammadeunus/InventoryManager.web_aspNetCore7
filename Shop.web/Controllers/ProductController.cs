6b6f8f4c3183fb44ccfde91ae7be29bf6818815d
mohammad eunus <100eunus@gmail.com>
2023-07-07 22:37:02 +0600

    Feature/products (#17) updated
    * applied repository pattern: By utilizing the Repository pattern, we have achieved a clear separation of concerns, improved code organization.
    
    * binded the class
    
    * changed controller name from ProductModelController to ProductController
    
    * product feature missing feature from stock (#16)
    
    * views documentation added
    
    * controller documentation added for IEnumerable vs List
    
    * 1
    
    * serilog configuuration
    
    * autofac configuration
    
    * grammar
    
    * Feature/product (#6)
    
    * renamed properties for product model
    
    * migration and view file added,
    error: need to ensure foreign key matches when inserting data
    
    * insertion in product table working
    
    * added exception handling
    
    * modelState not at right place
    
    * working: showing the category as list in frontend
    
    * product entry working.
    added: dropdown option for category in cshtml, annotation in model, and logged modelState validation failed reason
    
    * Feature/product (#7)
    
    * renamed properties for product model
    
    * migration and view file added,
    error: need to ensure foreign key matches when inserting data
    
    * insertion in product table working
    
    * added exception handling
    
    * modelState not at right place
    
    * working: showing the category as list in frontend
    
    * product entry working.
    added: dropdown option for category in cshtml, annotation in model, and logged modelState validation failed reason
    
    ---------
    
    Co-authored-by: mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
    
    * wrong naming; corrected
    
    * Feature/stock (#13)
    
    * solved SqlNullValueException error
    
    * working: added stock feature
    
    * customer feature added (#14)
    
    ---------
    
    Co-authored-by: AdminVai <83267669+MohammadEunusV2@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: AdminVai <83267669+MohammadEunusV2@users.noreply.github.com>

---
+++ Shop.web/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Shop.web.Models;
+using Shop.web.Repositories;
+
+namespace Shop.web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository _productRepository;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger, ProductRepository productRepository)
+        {
+            _logger = logger;
+            _productRepository = productRepository;
+        }
+
+        // GET: Product
+        // GET: /Product/ProductEntry
+        public IActionResult ProductEntry()
+        {
+            ViewBag.modList = _productRepository.PopulateCategoryListForDropDown();
+
+            return View();
+        }
+
+
+        public IActionResult SaveProduct(ProductModel model)
+        {
+            try
+            {
+                _logger.LogInformation("Product/SaveProduct action was called.");
+
+                if (ModelState.IsValid)
+                {
+                    _productRepository.AddProduct(model);
+                    ModelState.Clear();
+
+                    // Redirect to a different page after successful submission
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    _productRepository.LogValidationErrors(ModelState);
+
+                    // If the model state is not valid, return to the ProductEntry view with validation errors
+
+                    ViewBag.modList = _productRepository.PopulateCategoryListForDropDown();
+
+                    return View("ProductEntry", model);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred in Product/SaveProduct"); 
+                return View("Error");
+            }
+        }
+
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var products = _productRepository.GetAllProducts();
+                return View(products);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred in Product/Index"); 
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ Shop.web/Repository/ProductRepository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering; 
+using Shop.web.Data;
+using Shop.web.Models;
+
+namespace Shop.web.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ProductRepository> _logger;
+
+        public ProductRepository(ApplicationDbContext context, ILogger<ProductRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IEnumerable<ProductModel> GetAllProducts()
+        {
+            try
+            {
+                return _context.products.ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "ProductRepository.GetAllProducts: An error occurred while retrieving products.");
+                throw;
+            }
+        }
+
+        public void LogValidationErrors(ModelStateDictionary modelState)
+        {
+            try
+            {
+                _logger.LogInformation("ModelState is not valid: In ProductRepository/LogValidationErrors.");
+                foreach (var keyModelStatePair in modelState)
+                {
+                    var errors = keyModelStatePair.Value.Errors;
+                    if (errors != null && errors.Count > 0)
+                    {
+                        var key = keyModelStatePair.Key;
+                        var errorMessageArray = errors.Select(error => error.ErrorMessage).ToArray();
+
+                        var errorMessages = string.Join(", ", errorMessageArray);
+                        _logger.LogInformation($"ModelState is not valid: {key} has {errorMessages}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while logging validation errors.");
+                throw;
+            }
+        }
+
+        public void AddProduct(ProductModel model)
+        {
+            try
+            {
+                // Set the current time for CreatedDate and UpdatedDate properties
+                model.CreatedDate = DateTime.Now;
+                model.CreatedBy = "Eunus";
+
+                _context.products.Add(model);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "ProductRepository.AddProduct: An error occurred while saving the product.");
+                throw;
+            }
+        }
+
+        public List<SelectListItem> PopulateCategoryListForDropDown()
+        {
+            try
+            {
+                var modList = new List<SelectListItem>();
+                foreach (var category in _context.categories)
+                {
+                    modList.Add(new SelectListItem { Text = category.Name, Value = category.Id.ToString() });
+                }
+                return modList;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "ProductRepository.PopulateCategoryList: An error occurred while populating the category list.");
+                throw;
+            }
+        }
+    }
+}
