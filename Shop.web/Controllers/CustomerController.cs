4b569fc7fcfbf8b32ace9c43aa28a25310d3ff03
mohammad eunus <100eunus@gmail.com>
2023-07-07 22:34:41 +0600

    product feature missing feature from stock (#16)
    * views documentation added
    
    * controller documentation added for IEnumerable vs List
    
    * 1
    
    * serilog configuuration
    
    * autofac configuration
    
    * grammar
    
    * Feature/product (#6)
    
    * renamed properties for product model
    
    * migration and view file added,
    error: need to ensure foreign key matches when inserting data
    
    * insertion in product table working
    
    * added exception handling
    
    * modelState not at right place
    
    * working: showing the category as list in frontend
    
    * product entry working.
    added: dropdown option for category in cshtml, annotation in model, and logged modelState validation failed reason
    
    * Feature/product (#7)
    
    * renamed properties for product model
    
    * migration and view file added,
    error: need to ensure foreign key matches when inserting data
    
    * insertion in product table working
    
    * added exception handling
    
    * modelState not at right place
    
    * working: showing the category as list in frontend
    
    * product entry working.
    added: dropdown option for category in cshtml, annotation in model, and logged modelState validation failed reason
    
    ---------
    
    Co-authored-by: mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
    
    * wrong naming; corrected
    
    * Feature/stock (#13)
    
    * solved SqlNullValueException error
    
    * working: added stock feature
    
    * customer feature added (#14)
    
    ---------
    
    Co-authored-by: AdminVai <83267669+MohammadEunusV2@users.noreply.github.com>

---
+++ Shop.web/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Shop.web.Data;
+using Shop.web.Models;
+
+namespace Shop.web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public CustomerController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // GET: CustomerModels
+        // GET: /Customer/CustomerEntry
+        public IActionResult CustomerEntry()
+        { 
+            return View();
+        }
+
+        public IActionResult SaveCustomer(CustomerModel model)
+        {
+            _logger.LogInformation("CustomerModel/SaveCustomer action was called.");
+
+            // Set the current time for CreatedDate and UpdatedDate properties
+            model.CreatedDate = DateTime.Now;
+            model.CreatedBy = "Eunus";
+
+            ModelState.Clear();
+            if (ModelState.IsValid)
+            {
+                // Process and save the Customer data
+                _context.customers.Add(model);
+                _context.SaveChanges();
+
+                // Redirect to a different page after successful submission
+                return RedirectToAction("index");
+            }
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation("ModelState is not Valid: In CustomerModel/SaveCustomer.");
+                foreach (var modelStateEntry in ModelState.Values)
+                {
+                    foreach (var error in modelStateEntry.Errors)
+                    {
+                        _logger.LogInformation("ModelState is not Valid: " + error.ErrorMessage);
+                    }
+                }
+                ModelState.Clear();
+            }
+
+
+            // If the model state is not valid, return to the CustomerEntry view with validation errors
+            return View("CustomerEntry", model);
+        }
+
+
+        public IActionResult index()
+        {
+            return View(_context.customers);
+        }
+    }
+}
